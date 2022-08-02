70855bbfdd9312ec911963b14a209bbaa401424c
patrick letourneux <patrick.letourneux20@gmail.com>
2022-08-02 16:52:36 +0200

    to continue calculCombinationMAtchWall, need refacto

--- kata/Wardrobe.js
+++ kata/Wardrobe.js
@@ -1,7 +1,48 @@
 class Wardrobe {
-    log(){
-        console.log('OK class importée')
-        return 'OK'
+    wall
+    sizes = [50, 75, 100, 120]
+    constructor(wall){
+        this.wall = wall;
+    }
+    combinationsOK(){
+        console.log('OK class importée');
+        // return [];
+        return [[50,50,50,50,50]];
+    }
+    wallValue(){
+        return this.wall;
+    }
+    calculAllCombinations(){
+        const arrayCombination = []
+        for (let a=0;a<=5;a++){
+            for (let b=0;b<=5;b++){
+                for (let c=0;c<=5;c++){
+                    for (let d=0;d<=5;d++){
+                        const combination = [];
+                        for (let aa=a;aa>0;aa--){
+                            combination.push(this.sizes[0]);
+                        }
+                        for (let bb=b;bb>0;bb--){
+                            combination.push(this.sizes[1]);
+                        }
+                        for (let cc=c;cc>0;cc--){
+                            combination.push(this.sizes[2]);
+                        }
+                        for (let dd=d;dd>0;dd--){
+                            combination.push(this.sizes[3]);
+                        }
+                        arrayCombination.push(combination);
+
+                    }
+                }
+            }
+        }
+        console.log('arrayCombination.length ',arrayCombination.length);
+        return arrayCombination
+    }
+    calculCombinationMatchWall(){
+        const arrayCombination = this.calculAllCombinations() ;
+        return [[50,50,50,50,50]];
     }
 }
 
--- kata/Wardrobe.test.js
+++ kata/Wardrobe.test.js
@@ -1,18 +1,37 @@
 const Wardrobe = require('./Wardrobe');
 
 describe('Wardrobe class', () => {
-    
-    const instance = new Wardrobe();
+
+    const wall = 250
+    const instance = new Wardrobe(wall);
     
     beforeEach(() => {
         console.log('beforeEach');
       })
 
-      describe('log()', () => {
-    
-        test('log() return OK', () => {
-          expect(instance.log()).toEqual('OK');
+      describe('combinationsOK()', () => {   
+        test('combinationsOK return an Array', () => {
+          expect(instance.combinationsOK()).toEqual(expect.arrayContaining([[50,50,50,50,50]]));
         });
     
-    });
+      });
+      describe('wall()', () => { 
+        test('wall() should return wall', () => {
+          expect(instance.wallValue()).toEqual(wall);
+        });
+    
+      });
+      describe('calculAllCombinations()', () => { 
+        test('calculAllCombinations() should return ', () => {
+          expect(instance.calculAllCombinations()).toHaveLength(1296);
+          expect(instance.calculAllCombinations()).toEqual(expect.arrayContaining([[50,50,50,50,50]]));
+        });
+    
+      });
+      describe('calculCombinationMatchWall()', () => { 
+        test('calculCombinationMatchWall() should return ', () => {
+          expect(instance.calculCombinationMatchWall()).toEqual(expect.arrayContaining([[50,50,50,50,50]]));
+        });
+    
+      });
 })
